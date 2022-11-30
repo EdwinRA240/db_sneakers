c4a3cf85f32332310c25c78e87260ea7a18a78f6
Edwin RA <edwinra240@gmail.com>
2022-11-29 20:15:04 -0600

    feat: data in JSON and style with MUI

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,13 +1,24 @@
-import { React } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 import TableB from "../components/TableB";
 
 const Main = () => {
-  
+  const [employees, setemployees] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/employees")
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        setemployees(responseJson);
+      });
+  }, []);
+
   return (
     <>
       <NavBar />
-      <TableB />
+      <TableB data = {employees}/>
     </>
   );
 };
