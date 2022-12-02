7e4b5aa0ef80838fd5de7ddb041bd2523cf61836
Edwin RA <edwinra240@gmail.com>
2022-12-01 23:18:32 -0600

    feat:  add icons, routing in NavBar

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import NestedModal from "../components/Modal";
 import NavBar from "../components/NavBar";
 import TableB from "../components/TableB";
 
