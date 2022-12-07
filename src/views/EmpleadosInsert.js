b2ef3c875202dc290fc2688aa7dc3f232e999205
Edwin RA <edwinra240@gmail.com>
2022-12-06 19:06:32 -0600

    feat: redireccionamiento a insertEmpleados and struct

--- src/views/EmpleadosInsert.js
+++ src/views/EmpleadosInsert.js
@@ -1,17 +1,21 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import FormControl from "../components/FormControl";
+import UpgradeIcon from '@mui/icons-material/Upgrade';
 
 const EmpleadosInsert = () => {
   return (
     <>
-      <Container maxWidth="sm" sx={{ py: 4 }} >
+      <Container maxWidth="sm" sx={{ py: 4 }}>
         <TextField fullWidth label="fullWidth" id="fullWidth" />
-        <FormControl/>
-        <FormControl/>
-        <FormControl/>
-        <FormControl/>
+        <FormControl />
+        <FormControl />
+        <FormControl />
+        <FormControl />
+        <Button variant="contained" endIcon={<UpgradeIcon />}>
+          Send
+        </Button>
       </Container>
     </>
   );
